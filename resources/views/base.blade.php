8dbf67423e7b38e01bcd5de524f4d497de4e22ae
Okorin <okoratu@web.de>
2018-01-05 22:30:49 +0100

    Create basic layout using bootstrap

---
+++ resources/views/base.blade.php
@@ -0,0 +1,90 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="{{ url('/css/bootstrap.min.css') }}" crossorigin="anonymous">
+    <link rel="stylesheet" href="{{ url('/css/app.css') }}">
+
+    <link rel="icon" href="{{ url('/favicon.ico?v=2') }}" type="image/x-icon" />
+
+    <title>Hello, world!</title>
+  </head>
+<body>
+
+    <header>
+      <div class="fixed-top container-fluid bg-light headbar">
+        <div class="row align-items-center h-100">
+          <div class="col">
+            <h1 class="bold font-weight-bold">Community Mentorship Program</h1>
+          </div>
+          <div class="col">
+            <h1 class="float-right font-weight-bold">Login</h1>
+          </div>
+        </div>
+      </div>
+      <nav class="navbar navbar-expand-md fixed-top navbar-dark shadow-bottom">
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNav">
+          <ul class="nav navbar-nav nav-fill container-fluid">
+            <li class="nav-item active">
+              <a class="nav-link" href="#">home <span class="sr-only">(current)</span></a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">events</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">schedule</a>
+            </li>
+             <li class="nav-item dropdown">
+              <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                organisation
+              </a>
+              <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                <a class="dropdown-item" href="#">Action</a>
+                <a class="dropdown-item" href="#">Another action</a>
+                <div class="dropdown-divider"></div>
+                <a class="dropdown-item" href="#">Something else here</a>
+              </div>
+            </li>
+          </ul>
+        </div>
+      </nav>
+    </header>
+
+    <main role="main">
+      <div class="container h-100 complete-screen bg-light">
+        <div class="col extra-space h-100 shadow-left-and-right overflow complete-screen">
+          <h1>about</h1>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus efficitur aliquam semper. Donec erat nunc, mollis nec egestas auctor, elementum sit amet dui. Quisque nec aliquam tellus. Donec sagittis ornare lacus vitae consectetur. In laoreet finibus ex, quis porttitor massa aliquet suscipit. Integer consequat sodales ex, ut lobortis felis egestas sed. Sed pellentesque egestas volutpat.</p>
+<p>Donec ac ultrices risus. Mauris dapibus lacus ac feugiat fringilla. Quisque vestibulum lacinia cursus. Duis at libero nisl. Nunc nec dolor ac elit eleifend sagittis. Donec cursus, quam non elementum fermentum, enim elit venenatis justo, et mollis ligula mauris nec metus. Nullam massa ligula, rhoncus sed molestie non, elementum eget magna. Suspendisse tempus tortor vitae vestibulum rutrum. Maecenas ultrices quis neque in tincidunt. Aenean fermentum congue elit, et iaculis sem. Nam iaculis purus et finibus accumsan. Nullam bibendum ipsum varius, eleifend dui eu, cursus erat. Praesent vehicula lobortis mauris in euismod. Nulla fringilla ultricies neque ut commodo.</p>
+          <h1>news</h1>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus efficitur aliquam semper. Donec erat nunc, mollis nec egestas auctor, elementum sit amet dui. Quisque nec aliquam tellus. Donec sagittis ornare lacus vitae consectetur. In laoreet finibus ex, quis porttitor massa aliquet suscipit. Integer consequat sodales ex, ut lobortis felis egestas sed. Sed pellentesque egestas volutpat.</p>
+<p>Donec ac ultrices risus. Mauris dapibus lacus ac feugiat fringilla. Quisque vestibulum lacinia cursus. Duis at libero nisl. Nunc nec dolor ac elit eleifend sagittis. Donec cursus, quam non elementum fermentum, enim elit venenatis justo, et mollis ligula mauris nec metus. Nullam massa ligula, rhoncus sed molestie non, elementum eget magna. Suspendisse tempus tortor vitae vestibulum rutrum. Maecenas ultrices quis neque in tincidunt. Aenean fermentum congue elit, et iaculis sem. Nam iaculis purus et finibus accumsan. Nullam bibendum ipsum varius, eleifend dui eu, cursus erat. Praesent vehicula lobortis mauris in euismod. Nulla fringilla ultricies neque ut commodo.</p>
+          <h2>something else</h2>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus efficitur aliquam semper. Donec erat nunc, mollis nec egestas auctor, elementum sit amet dui. Quisque nec aliquam tellus. Donec sagittis ornare lacus vitae consectetur. In laoreet finibus ex, quis porttitor massa aliquet suscipit. Integer consequat sodales ex, ut lobortis felis egestas sed. Sed pellentesque egestas volutpat.</p>
+<p>Donec ac ultrices risus. Mauris dapibus lacus ac feugiat fringilla. Quisque vestibulum lacinia cursus. Duis at libero nisl. Nunc nec dolor ac elit eleifend sagittis. Donec cursus, quam non elementum fermentum, enim elit venenatis justo, et mollis ligula mauris nec metus. Nullam massa ligula, rhoncus sed molestie non, elementum eget magna. Suspendisse tempus tortor vitae vestibulum rutrum. Maecenas ultrices quis neque in tincidunt. Aenean fermentum congue elit, et iaculis sem. Nam iaculis purus et finibus accumsan. Nullam bibendum ipsum varius, eleifend dui eu, cursus erat. Praesent vehicula lobortis mauris in euismod. Nulla fringilla ultricies neque ut commodo.</p>
+<p class="mb-0">Donec ac ultrices risus. Mauris dapibus lacus ac feugiat fringilla. Quisque vestibulum lacinia cursus. Duis at libero nisl. Nunc nec dolor ac elit eleifend sagittis. Donec cursus, quam non elementum fermentum, enim elit venenatis justo, et mollis ligula mauris nec metus. Nullam massa ligula, rhoncus sed molestie non, elementum eget magna. Suspendisse tempus tortor vitae vestibulum rutrum. Maecenas ultrices quis neque in tincidunt. Aenean fermentum congue elit, et iaculis sem. Nam iaculis purus et finibus accumsan. Nullam bibendum ipsum varius, eleifend dui eu, cursus erat. Praesent vehicula lobortis mauris in euismod. Nulla fringilla ultricies neque ut commodo.</p>
+        </div>
+      <!-- FOOTER -->
+    </main>
+      <footer class="container-fluid bg-dark cust-footer text-light shadow-top-inset">
+        <div class="pt-3">
+          <p class="float-right"><a href="#">Back to top</a></p>
+          <p>Generic Disclaimer &middot; <a href="#">Privacy</a> &middot; <a href="#">Terms</a></p>
+        </div>
+      </footer>
+    </div>
+    <!-- Bootstrap core JavaScript
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+    <script>window.jQuery || document.write('<script src="{{ url('/js/jquery-slim.min.js') }}"><\/script>')</script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+    <script src="{{ url('/js/bootstrap.min.js') }}" integrity="sha384-a5N7Y/aK3qNeh15eJKGWxsqtnX/wWdSZSKp+81YjTmS15nvnvxKHuzaWwXHDli+4" crossorigin="anonymous"></script>
+  </body>
+</html>
