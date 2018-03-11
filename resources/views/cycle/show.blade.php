e35f81e3639d00cc5d6e4f21ef7acffde94b4b54
unknown <max100@live.se>
2018-03-11 20:36:56 +0100

    Design for cycles (edit.blade.php) (show.blade.php)

--- resources/views/cycle/show.blade.php
+++ resources/views/cycle/show.blade.php
@@ -1,17 +1,29 @@
 @extends('layouts.master')
 
 @section('title', 'Cycle ' . $cycle->id)
-
+    <style>
+	.border-right {
+        border-right: 2px solid #e9ecef !important;
+    }
+	</style>
 @section('content')
-	<h2>Cycle {{ $cycle->id }}</h2>
-    @foreach($modeparticipants as $key => $participants)
-        @if(!$participants->isEmpty())
-            {{ $gamemodes[$key-1]->name }}
-        @endif
-    	@foreach($participants as $participant) 
-            <a href="{{ route('user.show', $participant->user_id) }}" style="color:#{{ $participant->role->color }}"> 
-            {{ $participant->user->name }}
-            </a>
+	<div class="row justify-content-center">
+	    <div class="d-inline-flex p-3">
+	        <h1>Cycle {{ $cycle->id }}</h1>
+		</div>
+	</div>
+	<div class="row">
+    	@foreach($modeparticipants as $key => $participants)
+            @if(!$participants->isEmpty())
+                <div class="col-6 border-right" style="text-align:center;padding:0.2em;"><h2>{{ $gamemodes[$key-1]->name }}</h2></div>
+            @endif
+			<div class="col-6 divGamemode">
+           	@foreach($participants as $participant) 	
+	       	    <a href="{{ route('user.show', $participant->user_id) }}" style="color:#{{ $participant->role->color }}"> 
+                    {{ $participant->user->name }}
+                </a>
+	        @endforeach
+			</div>
         @endforeach
-    @endforeach
+	</div>
 @endsection
